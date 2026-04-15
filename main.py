a901f9695150404b9232969806ba90b638e12d45
bhuvidatascience-dotcom <bhuvidatascience@gmail.com>
2026-04-16 00:34:28 +0530

    fastapi deployment

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+from preprocess import clean_text
+app = FastAPI()
+Mod_path="/home/bhuvaneshwaran/Documents/Documents/DataScience/DS CLASS/NLP/Log_Reg_Sentimentmodel.pkl"
+Vec_path="/home/bhuvaneshwaran/Documents/Documents/DataScience/DS CLASS/NLP/Log_Reg_Sentimentvect.pkl"
+# Load model+vectorizer
+model = pickle.load(open(Mod_path, "rb"))
+vectorizer = pickle.load(open(Vec_path, "rb"))
+
+class Comment(BaseModel):
+    text: str
+
+@app.get("/")
+def home():
+    return {"message": "API running"}
+
+@app.post("/predict")
+def predict(data: Comment):
+    try:
+        cleaned = clean_text(data.text)
+        transformed = vectorizer.transform([cleaned])
+        prediction = model.predict(transformed)[0]
+        label_map = {0: "Negative", 1: "Neutral",2:"Positive"}
+        # return {"sentiment": int(prediction)}   # ✅ FIX
+        return {"sentiment": label_map[int(prediction)]}
+
+    except Exception as e:
+        return {"error": str(e)}
---
+++ preprocess.py
@@ -0,0 +1,19 @@
+import re
+import string
+
+def clean_text(text):
+    text = text.lower()
+    
+    # remove URLs
+    text = re.sub(r"http\S+", "", text)
+    
+    # remove punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    
+    # remove numbers
+    text = re.sub(r'\d+', '', text)
+
+    # remove extra spaces
+    text = text.strip()
+
+    return text
